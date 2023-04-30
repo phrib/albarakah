86e793411dcae7d4db2ae9c52a8e4fb1e2332485
phrib <132211674+phrib@users.noreply.github.com>
2023-04-30 11:31:50 -0300

    Add files via upload

---
+++ BaseTemplate.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        dots: true,
+        pauseOnFocus: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: true,
+        speed: 500,
+        cssEase: 'linear'
+    });
+});
